1386d495374a0f8b402c8b0ea752b9d7b6ac29da
Bilginer Oral <bilgineroral@gmail.com>
2024-04-16 16:10:02 +0300

    add VAE impl.

---
+++ ResNetVAE/decoder.py
@@ -0,0 +1,47 @@
+class Decoder(nn.Module):
+    def __init__(self, fc_hidden2=768, CNN_embed_dim=256):
+        super(Decoder, self).__init__()
+
+        self.fc_hidden2, self.CNN_embed_dim = fc_hidden2, CNN_embed_dim
+
+        self.ch1, self.ch2, self.ch3, self.ch4 = 16, 32, 64, 128
+        self.k1, self.k2, self.k3, self.k4 = (5, 5), (3, 3), (3, 3), (3, 3)      # 2d kernal size
+        self.s1, self.s2, self.s3, self.s4 = (2, 2), (2, 2), (2, 2), (2, 2)      # 2d strides
+        self.pd1, self.pd2, self.pd3, self.pd4 = (0, 0), (0, 0), (0, 0), (0, 0)  # 2d padding
+
+        # Sampling vector
+        self.fc4 = nn.Linear(self.CNN_embed_dim, self.fc_hidden2)
+        self.fc_bn4 = nn.BatchNorm1d(self.fc_hidden2)
+        self.fc5 = nn.Linear(self.fc_hidden2, 64 * 4 * 4)
+        self.fc_bn5 = nn.BatchNorm1d(64 * 4 * 4)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Decoder
+        self.convTrans6 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=self.k4, stride=self.s4,
+                               padding=self.pd4),
+            nn.BatchNorm2d(32, momentum=0.01),
+            nn.ReLU(inplace=True),
+        )
+        self.convTrans7 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=32, out_channels=8, kernel_size=self.k3, stride=self.s3,
+                               padding=self.pd3),
+            nn.BatchNorm2d(8, momentum=0.01),
+            nn.ReLU(inplace=True),
+        )
+
+        self.convTrans8 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=8, out_channels=3, kernel_size=self.k2, stride=self.s2,
+                               padding=self.pd2),
+            nn.BatchNorm2d(3, momentum=0.01),
+            nn.Sigmoid()    # y = (y1, y2, y3) \in [0 ,1]^3
+        )
+
+    def forward(self, z):
+        x = self.relu(self.fc_bn4(self.fc4(z)))
+        x = self.relu(self.fc_bn5(self.fc5(x))).view(-1, 64, 4, 4)
+        x = self.convTrans6(x)
+        x = self.convTrans7(x)
+        x = self.convTrans8(x)
+        x = F.interpolate(x, size=(224, 224), mode='bilinear')
+        return x
